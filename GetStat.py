5824ec3ab19fd6343d5d6131f61b5508da2479a9
levon-fischer <levon.fischer@gmail.com>
2023-01-24 22:19:06 -0500

    Add Seasons Per Owner function for future construction of Prestige Rankings

--- GetStat.py
+++ GetStat.py
@@ -5,15 +5,24 @@ import PrestigeData
 # INPUT: LHDB dataframe
 # OUTPUT: tuple with the UID of the highest scorer and their total score
 @st.cache
-def get_highest_scorer(df):
+def highest_scorer(df):
     # take the LHDB and get just a single list of all the scores for each user
-    df = PrestigeData.get_ind_matchup_scores(df)
+    df = PrestigeData.ind_matchup_scores(df)
 
     # group the users and get their total scores
     grouped_users = df.groupby('uid')['score'].sum()
 
     # get the
-    highest_scorer = grouped_users.idxmax()
-    score = grouped_users[highest_scorer]
+    high_scorer = grouped_users.idxmax()
+    score = grouped_users[high_scorer]
 
-    return highest_scorer, score
+    return high_scorer, score
+
+
+# INPUT: Individual matchups dataframe
+# OUTPUT: Dictionary with the number of seasons mapped to the UID
+@st.cache
+def seasons_per_owner(df):
+    num_seasons = df.groupby('uid')['year'].nunique().to_dict()
+
+    return num_seasons
